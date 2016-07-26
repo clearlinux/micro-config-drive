2c7ba9a93492444af6fb62e195144f99fd603727
Ikey Doherty <michael.i.doherty@intel.com>
2016-07-26 13:08:01 +0100

    Replace all instances of clr-cloud-init with micro-config-drive
    The project has been renamed from clr-cloud-init to clearly communicate the
    scope and target of the project. This change simply ensures all the source
    files are now consistent in their agreement of the project name.
    
    The (C) usage was also swapped for ©, simply because it is the proper symbol
    and looks better.
    
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- src/interpreters/cloud_config.c
+++ src/interpreters/cloud_config.c
@@ -1,23 +1,23 @@
 /***
- Copyright (C) 2015 Intel Corporation
+ Copyright © 2015 Intel Corporation
 
  Author: Auke-jan H. Kok <auke-jan.h.kok@intel.com>
  Author: Julio Montes <julio.montes@intel.com>
 
- This file is part of clr-cloud-init.
+ This file is part of micro-config-drive.
 
- clr-cloud-init is free software: you can redistribute it and/or modify
+ micro-config-drive is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
 
- clr-cloud-init is distributed in the hope that it will be useful,
+ micro-config-drive is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with clr-cloud-init. If not, see <http://www.gnu.org/licenses/>.
+ along with micro-config-drive. If not, see <http://www.gnu.org/licenses/>.
 
  In addition, as a special exception, the copyright holders give
  permission to link the code of portions of this program with the
