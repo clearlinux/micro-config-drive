7ed37b2b41fcc43cb48d6a02c9f03e21072c13d4
jwang <jing.j.wang@intel.com>
2019-09-13 11:48:10 +0800

    fix typo in tencent cloud-config template

--- src/ucd-data-fetch.c
+++ src/ucd-data-fetch.c
@@ -101,7 +101,7 @@ static struct cloud_struct config[MAX_CONFIGS] = {
 		"users:\n" \
 		"  - name: tencent\n" \
 		"    groups: wheelnopw\n" \
-		"ssh-authorized-keys:\n" \
+		"ssh_authorized_keys:\n" \
 		"  - "
 	}
 };
